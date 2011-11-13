4a0606581bdb9df9b7f223e937fb81dcd862b8dc
Chris Sharp <info@modelspoorgroepvenlo.nl>
2011-11-13 18:54:52 +0000

    Added easy connector modes for MGV133 & MGV145

--- Source/Devices/LocoIO/ConnectorMode.Statics.cs
+++ Source/Devices/LocoIO/ConnectorMode.Statics.cs
@@ -8,7 +8,11 @@ namespace LocoNetToolBox.Devices.LocoIO
         public static readonly ConnectorMode Mgv77 = new ConnectorModes.Mgv77();
         public static readonly ConnectorMode Mgv93 = new ConnectorModes.Mgv93();
         public static readonly ConnectorMode Mgv102 = new ConnectorModes.Mgv102();
+        public static readonly ConnectorMode Mgv133LowPins = new ConnectorModes.Mgv133LowPins();
+        public static readonly ConnectorMode Mgv133HighPins = new ConnectorModes.Mgv133HighPins();
         public static readonly ConnectorMode Mgv136 = new ConnectorModes.Mgv136();
+        public static readonly ConnectorMode Mgv145J5 = new ConnectorModes.Mgv145J5();
+        public static readonly ConnectorMode Mgv145J6 = new ConnectorModes.Mgv145J6();
 
         private static readonly ConnectorMode[] Modes = new[]
         {
@@ -16,7 +20,11 @@ namespace LocoNetToolBox.Devices.LocoIO
             Mgv77,
             Mgv93, 
             Mgv102, 
+            Mgv133LowPins,
+            Mgv133HighPins,
             Mgv136, 
+            Mgv145J5,
+            Mgv145J6,
             new ConnectorModes.Mgv81V1(), 
         };
 
---
+++ Source/Devices/LocoIO/ConnectorModes/Mgv133LowPins.cs
@@ -0,0 +1,40 @@
+﻿namespace LocoNetToolBox.Devices.LocoIO.ConnectorModes
+{
+    /// <summary>
+    /// Connector mode for an MGV133 with sensors connected to pin 1-4.
+    /// </summary>
+    public sealed class Mgv133LowPins : ConnectorMode
+    {
+        /// <summary>
+        /// Default ctor
+        /// </summary>
+        public Mgv133LowPins()
+            : base("MGV133 (pin 1-4)",
+                4,
+                new[] {
+                    "Sensor 1",
+                    "Sensor 2",
+                    "Sensor 3",
+                    "Sensor 4",
+                },
+                PinMode.BlockActiveLowDelay,
+                PinMode.BlockActiveLowDelay,
+                PinMode.BlockActiveLowDelay,
+                PinMode.BlockActiveLowDelay)
+        {
+        }
+
+        /// <summary>
+        /// Use this mode to configure the given target.
+        /// </summary>
+        protected override void Configure(ConnectorConfig target, AddressList addresses)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                var pin = target.Pins[i];
+                pin.Mode = GetPin(i);
+                pin.Address = addresses[i];
+            }
+        }
+    }
+}
